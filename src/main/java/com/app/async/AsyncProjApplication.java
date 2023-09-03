9a2f816c9e80b879b7652c72b8858650b0fdff69
LimJiNa <jn4624@gmail.com>
2023-09-03 22:52:43 +0900

    initial commit

---
+++ src/main/java/com/app/async/AsyncProjApplication.java
@@ -0,0 +1,13 @@
+package com.app.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/async/AsyncProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
